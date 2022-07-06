7f07192e8442e8f7fb6239df0b5e15ee8809208f
SilvestruGit <silvestru.state17@gmail.com>
2022-07-06 16:20:22 +0300

    pauza

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,42 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Custom user manager."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create save and return a new user."""
+        user = self.model(email=self._normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def _normalize_email(self, email):
+        """Returns normalized emails."""
+        email = email.split('@')
+        email = email[0] + '@' + email[1].lower()
+        return email
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
---
+++ app/core/tests/test_custom_user_model.py
@@ -0,0 +1,34 @@
+"""
+Tests for the custom user model.
+"""
+
+from django.contrib.auth import get_user_model
+
+from django.test import TestCase
+
+
+class UserTests(TestCase):
+    """Tests for the custom user model."""
+
+    def test_create_user(self):
+        """Test creating a user is succesful."""
+
+        email = 'test@example.com'
+        password = 'parola1234'
+
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_normalize_email(self):
+        """Test normalize email adresses."""
+        emails = [
+            ['TEST@EXAMPLE.COM', 'TEST@example.com'],
+            ['Test@examPle.com', 'Test@example.com'],
+            ['test@example.COM', 'test@example.com'],
+        ]
+
+        for email, expected in emails:
+            user = get_user_model().objects.create_user(email)
+            self.assertEqual(user.email, expected)
