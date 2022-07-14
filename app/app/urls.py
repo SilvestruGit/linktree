641785a704b2624e33a3197e5a761850190aeded
SilvestruGit <silvestru.state17@gmail.com>
2022-07-14 20:16:56 +0300

    done

--- app/app/urls.py
+++ app/app/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
         name='api-docs',
     ),
     path('api/user/', include('user.urls')),
-    path('api/linktree/', include('linktree.urls'))
+    path('api/linktree/', include('linktree.urls')),
 ]
